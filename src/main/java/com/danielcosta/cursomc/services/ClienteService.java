5389014652c7ad3ea5885361f1c88195f9cde5b9
NewDaniC <danieltheisgesdacosta@gmail.com>
2023-03-14 19:01:22 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/danielcosta/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.danielcosta.cursomc.services;
+
+
+import java.util.Optional;
+
+import com.danielcosta.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielcosta.cursomc.domain.Cliente;
+import com.danielcosta.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
