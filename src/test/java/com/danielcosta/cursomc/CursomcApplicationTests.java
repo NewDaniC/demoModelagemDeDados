ee653dabeb69c603c05026a1c1e11067afacea84
NewDaniC <danieltheisgesdacosta@gmail.com>
2023-03-14 11:15:27 -0300

    Projeto Criado

---
+++ src/test/java/com/danielcosta/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielcosta.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
